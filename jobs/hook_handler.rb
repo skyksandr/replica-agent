7fb8dcfa4fe2bfeddaf1f4fc905070d3f92f1753
Aleksandr Kunin <ab@ipkunin.ru>
2016-10-31 11:04:04 +0300

    improvements to hook handler

--- jobs/hook_handler.rb
+++ jobs/hook_handler.rb
@@ -2,13 +2,12 @@ require 'webrick'
 
 module Jobs
   class HookHandler
-    KEY = 'secret_key'
-
     def start
       @server = WEBrick::HTTPServer.new Port: 10080
 
       @server.mount_proc '/' do |request, _|
-        next unless request.request_uri.path == "/#{KEY}"
+        next unless request.request_uri.path == "/#{config['hook_key']}"
+        next unless hook_valid?(request.body)
         handle_hook
       end
 
@@ -22,7 +21,15 @@ module Jobs
     private
     
     def handle_hook
-      notify('handle hook')
+      notify('New image pushed to Docker Hub. Pulling...')
+      %x( docker pull #{config['image_name']}:#{config['image_tag']} )
+      notify('Deploying latest backup to staging db')
+      notify('Deploying app container')
+    end
+
+    def hook_valid?(req_body)
+      req_hash = JSON.parse(req_body)
+      req_hash.dig('push_data', 'tag') == config['imgage_tag']
     end
   end
 end
